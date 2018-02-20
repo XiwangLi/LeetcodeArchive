8bd57d3c3877a8174dd38ffe22b99d21017c2c17
XiwangLi <xiwangli2010@gmail.com>
2018-02-19 23:23:29 -0800

    added PathSum III Leetcode 437

--- DFS/PathSum.py
+++ DFS/PathSum.py
@@ -33,3 +33,25 @@ class Solution(object):
             return
         self.DFSpathSum(root.left, path + [root.val], sum - root.val, res) 
         self.DFSpathSum(root.right, path + [root.val], sum - root.val, res)
+
+#PathSum III
+
+class Solution(object):
+    def pathSum(self, root, sum):
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: int
+        """
+        if not root: return 0
+        res = 0
+        return self.DFSpathSum(root, sum) + self.pathSum(root.left, sum) + self.pathSum(root.right, sum) 
+        
+    def DFSpathSum(self, root, sum):
+        if not root:
+            return 0
+        res = 0
+        if sum == root.val: res += 1
+        res += self.DFSpathSum(root.left, sum - root.val)
+        res += self.DFSpathSum(root.right, sum - root.val)
+        return res
