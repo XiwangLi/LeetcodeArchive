8e1df059f34d685cf85723125134feada436973a
XiwangLi <xiwangli2010@gmail.com>
2018-02-19 22:59:46 -0800

    Update nextPermutations.py

--- DFS/nextPermutations.py
+++ DFS/nextPermutations.py
@@ -1,5 +1,5 @@
 # NextPermutation
-# This is not a DFS problem, but related with Permutation, so I discuss it here
+# This is not a DFS problem, but related with Permutation, so I put it here
 # The method is hard to think of but the code is easy. 
 #The key is to find out the index, i, where nums[i+1] > nums[i] and find the last index, j where nums[j] > nums[i]
 class Solution(object):
