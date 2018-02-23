36103413774611754d4b506a14c7399a00cba43f
xiwanglienergy <xiwangli2010@gmail.com>
2018-02-23 00:12:04 -0800

    Divide and Conquer

--- DFS/BinaryTreeFlatten.py
+++ DFS/BinaryTreeFlatten.py
@@ -17,3 +17,21 @@ class Solution(object):
 				tail = tail.right
 			root.left, root.right, tail.right = None, root.left, right
 		self.flatten(root.right)
+
+
+class Solution(object):  # Divide & Conquer
+    def flatten(self, root):
+        """
+        :type root: TreeNode
+        :rtype: void Do not return anything, modify root in-place instead.
+        """
+        if not root: return
+        self.flatten(root.left)  #flat left subtree
+        self.flatten(root.right) # flat right subtree
+        node = root
+        if not node.left:
+            return
+        node = node.left  
+        while node.right: #find the tail of left tree
+            node = node.right
+        node.right, root.left, root.right  = root.right, None, root.left  #flat the smallest tree
